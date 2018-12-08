8e699a0604b19499112d9acb877497297b9d5c6b
qq1030614192 <31330343+qq1030614192@users.noreply.github.com>
2018-12-08 18:11:30 +0800

    Add files via upload

---
+++ cal/cal/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace cal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(489, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 60);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "=";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Buttonop_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(260, 141);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(61, 60);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "÷";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Buttonop_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(164, 141);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(61, 60);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "×";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Buttonop_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(260, 12);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(61, 60);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "-";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Buttonop_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(164, 12);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(61, 60);
+            this.button13.TabIndex = 15;
+            this.button13.Text = "+";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.Buttonop_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 25);
+            this.textBox1.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(305, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 25);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(211, 92);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(53, 25);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(586, 92);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(160, 25);
+            this.textBox4.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(583, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(489, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 60);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "AC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Re_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 240);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "计算器-windows实验";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ cal/cal/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+namespace cal
+{
+    public partial class Form1 : Form
+    {
+        //dll的导入
+        [DllImport("Dll1.dll")]
+        public static extern double add(double a, double b);
+        [DllImport("Dll1.dll")]
+        public static extern double subtract(double a, double b);
+        [DllImport("Dll1.dll")]
+        public static extern double multiply(double a, double b);
+        [DllImport("Dll1.dll")]
+        public static extern double divide(double a, double b);
+
+        private string o;//运算符
+        private double x, y;//运算数
+        private Button btn;//按钮对象
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_load(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            label1.Text = "";
+        }
+        private void Buttonop_Click(object sender, EventArgs e)//运算符点击事件
+        {
+            btn = (Button)sender;
+            if (btn.Name!="button3")//点击四则运算按钮
+            {
+                o = btn.Name;
+                switch (o)//显示运算符
+                {
+                    //+
+                    case "button13":
+                        textBox3.Text = "+";
+                        break;
+                    //-
+                    case "button9":
+                        textBox3.Text = "-";
+                        break;
+                    //*
+                    case "button5":
+                        textBox3.Text = "×";
+                        break;
+                    ///
+                    case "button4":
+                        textBox3.Text = "÷";
+                        break;
+
+                }
+                //确保可以转化为double类型且不为空字符串
+                bool xtodouble = Regex.IsMatch(textBox1.Text, @"^[+-]?\d*[.]?\d*$");
+                bool ytodouble = Regex.IsMatch(textBox2.Text, @"^[+-]?\d*[.]?\d*$");
+                if (xtodouble&& ytodouble&& textBox1.Text!=""&& textBox2.Text!="") {
+                    x = Convert.ToDouble(textBox1.Text);
+                    y = Convert.ToDouble(textBox2.Text);
+                }
+                else
+                {
+                    label1.Text = "请输入正确数值";
+                    textBox4.Text = "!";
+                }
+               
+            }
+            else//点击“=”按钮
+            {
+                //同上检验
+                bool xtodouble = Regex.IsMatch(textBox1.Text, @"^[+-]?\d*[.]?\d*$");
+                bool ytodouble = Regex.IsMatch(textBox2.Text, @"^[+-]?\d*[.]?\d*$");
+                if (xtodouble && ytodouble && textBox1.Text != "" && textBox2.Text != "")
+                {
+                    x = Convert.ToDouble(textBox1.Text);
+                    y = Convert.ToDouble(textBox2.Text);
+                    switch (o)
+                    {
+                        //+
+                        case "button13":
+                            textBox4.Text = (add(x,y).ToString());
+                            label1.Text = "";
+                            break;
+                        //-
+                        case "button9":
+                            textBox4.Text = (subtract(x,y)).ToString();
+                            label1.Text = "";
+                            break;
+                        //*
+                        case "button5":
+                            textBox4.Text = (multiply(x,y)).ToString();
+                            label1.Text = "";
+                            break;
+                        ///
+                        case "button4":
+                            if (y == 0)
+                            {
+                                label1.Text = "请输入正确数值";
+                                textBox4.Text = "!";
+                            }
+                            else
+                            {
+                                textBox4.Text = (divide(x,y)).ToString();
+                                label1.Text = "";
+                            }
+                            break;
+
+                    }
+                }
+                else
+                {
+                    label1.Text = "请输入正确数值";
+                    textBox4.Text = "!";
+                }
+            }
+        }
+        private void Re_Click(object sender, EventArgs e)
+        {
+            //全部清零
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+    }
+}
